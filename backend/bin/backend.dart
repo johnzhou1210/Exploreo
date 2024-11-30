9d4b81caaea07120f4f91b6eea6dbaaac91111be
Eric Lam <lamericcollege@gmail.com>
2024-11-30 11:31:27 -0500

    Refactor backend server setup and update user API structure

--- backend/bin/backend.dart
+++ backend/bin/backend.dart
@@ -1,21 +1,42 @@
-import 'dart:io';
 
 import "package:shelf_router/shelf_router.dart";
 import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart' as io;
-
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
 import 'package:backend/user_api.dart';
-const _hostname = 'localhost';
-const _port = 8080;
 
-void main(List<String> args) async {
+// const _hostname = 'localhost';
+// const _port = 8080;
+
+// void main(List<String> args) async {
+//   var app = Router();
+//   app.mount('/user/', userApi().router);
+//   app.get('/hello', (Request request) {
+//     Response.ok('Hello hit backend');
+//   });
+//   final server = await shelf_io.serve(app, _hostname, 8080);
+//   print('Serving at httt://${server.address.host}:${_port}');
+// }
+
+void main() async {
   var app = Router();
-  app.mount('/user/', userApi().router);
+
+  app.mount('/user', UserApi().router.call);
   app.get('/hello', (Request request) {
-    Response.ok('Hello hit backend');
+    return Response.ok('Hello hit backend');
   });
-  final server = await io.serve(
-      app, _hostname, 8080);
-  print('Serving at httt://${server.address.host}:${_port}');
+
+  app.all('/<path|.*>', _echoRequest);
+  var handler =
+      const Pipeline().addMiddleware(logRequests()).addHandler(app.call);
+
+  var server = await shelf_io.serve(handler, 'localhost', 8080);
+
+  // Enable content compression
+  server.autoCompress = true;
+
+  print('Serving at http://${server.address.host}:${server.port}');
 }
+
+Response _echoRequest(Request request) =>
+    Response.ok('Request for "${request.url}"');
--- backend/lib/user_api.dart
+++ backend/lib/user_api.dart
@@ -1,9 +1,14 @@
+import "package:shelf/shelf.dart";
 import "package:shelf_router/shelf_router.dart";
 
-class userApi {
+class UserApi {
   Router get router {
     final router = Router();
 
+    router.get('/', (Request request) {
+      return Response.ok('Hello from user api');
+    });
+    
     return router;
   }
 }
