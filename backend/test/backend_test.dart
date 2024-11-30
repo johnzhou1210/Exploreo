7c33b0a2317e8e5bf75044b32d375278e6b91780
Eric Lam <lamericcollege@gmail.com>
2024-11-30 11:26:32 -0500

    Comment out test code in backend_test.dart

--- backend/test/backend_test.dart
+++ backend/test/backend_test.dart
@@ -1,8 +1,8 @@
-import 'package:backend/backend.dart';
-import 'package:test/test.dart';
+// import 'package:backend/backend.dart';
+// import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
