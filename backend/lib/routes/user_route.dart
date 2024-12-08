c44741fef2c4786876796be4970393de677f43f7
Eric Lam <lamericcollege@gmail.com>
2024-12-08 15:20:22 -0500

    Implement createUser endpoint in UserRoute with validation and error handling

--- backend/lib/routes/user_route.dart
+++ backend/lib/routes/user_route.dart
@@ -1,3 +1,6 @@
+import 'package:backend/prisma/generated_dart_client/prisma.dart';
+import 'package:backend/prisma/generated_dart_client/model.dart';
+import 'package:orm/orm.dart';
 import "package:shelf/shelf.dart";
 import "package:shelf_router/shelf_router.dart";
 import 'package:backend/prisma.dart';
@@ -24,9 +27,44 @@ class UserRoute {
       }
     }
 
+    Future<Response> createUser(Request request) async {
+      try {
+        final payload = jsonDecode(await request.readAsString());
+
+        // validate payload
+        if (payload == null ||
+            payload['email'] == null ||
+            payload['providerId'] == null ||
+            (payload['loginType'] == null &&
+                !!LoginType.values.any(
+                    (loginType) => loginType.name == payload['loginType']))) {
+          return Response(400,
+              body: json.encode({'error': 'Missing required fields'}));
+        }
+        final user = await prisma.user.create(
+            data: PrismaUnion.$1(
+          UserCreateInput(
+              email: payload['email'],
+              username: PrismaUnion.$1(payload['username']),
+              password: PrismaUnion.$1(payload['password']),
+              providerId: payload['providerId'],
+              loginType: LoginType.values.firstWhere(
+                  (loginType) => loginType.name == payload['loginType'])),
+        ));
+
+        return Response.ok(
+          json.encode(user.toJson()),
+          headers: {'Content-Type': 'application/json'},
+        );
+      } catch (e) {
+        print(e);
+        return Response(400, body: 'INTERNAL_SERVER_ERROR');
+      }
+    }
+
     router.get('/', getAllUsers);
     // router.get('/<id>', _getUserById);
-    // router.post('/', _createUser);
+    router.post('/', createUser);
     // router.put('/<id>', _updateUser);
     // router.delete('/<id>', _deleteUser);
 
